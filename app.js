d146c02bd9b380c9328c2f4f7855f213cf091157
DuoJay <82710377+DuoJay@users.noreply.github.com>
2022-02-05 21:46:43 +0100

    Changed Flex
    Changed lot of flex and pictures.
    CSS needs cleaning

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const mobileNav = document.querySelector('.mobile-nav');
 const menuBurger = document.querySelector('.menu-burger');
+const mobileNavItems = document.querySelectorAll('.mobile-nav__item')
 
-function showMobileNav(){
+function toggleMobileNav(){
     mobileNav.classList.toggle('toggle-mobile-nav')
 }
 
-menuBurger.addEventListener('click', showMobileNav)
+
+
+menuBurger.addEventListener('click', toggleMobileNav);
+mobileNavItems.forEach(mobileNavItem  => mobileNavItem.addEventListener('click',toggleMobileNav));
