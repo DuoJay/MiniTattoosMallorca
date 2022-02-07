d8fce8f02f5fe0cedfc92aa5e8f069c8cbbe2774
DuoJay <82710377+DuoJay@users.noreply.github.com>
2022-02-07 15:22:22 +0100

    Added Form Worked on Images
    Worked on images but didn't figure out yet how to make the closing of images look good.
    Form style is just temporary.

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const mobileNav = document.querySelector('.mobile-nav');
 const menuBurger = document.querySelector('.menu-burger');
-const mobileNavItems = document.querySelectorAll('.mobile-nav__item')
+const mobileNavItems = document.querySelectorAll('.mobile-nav__item');
+const backdrop = document.querySelector('.backdrop');
+const galleryImages = document.querySelectorAll('.insta-image');
 
 function toggleMobileNav(){
-    mobileNav.classList.toggle('toggle-mobile-nav')
+    mobileNav.classList.toggle('toggle-mobile-nav');
+    menuBurger.classList.toggle('opened');
+    toggleBackdrop();
+}
+
+function toggleBackdrop (){
+    backdrop.classList.toggle('toggle-backdrop');
+    setInterval(()=> backdrop.style.display = ('block'),400);
 }
 
 
-
+backdrop.addEventListener('click', toggleMobileNav);
 menuBurger.addEventListener('click', toggleMobileNav);
-mobileNavItems.forEach(mobileNavItem  => mobileNavItem.addEventListener('click',toggleMobileNav));
+mobileNavItems.forEach(item => item.addEventListener('click',toggleMobileNav));
+
+
+function enlargeImage (image){
+    image.classList.toggle('close-image');
+    image.classList.toggle('open-image');
+}
+
+
+function displayNone (){
+    galleryImages.forEach((image => 
+        image.classList.toggle('close-image'))
+    )}
+
+galleryImages.forEach(image => {
+    image.addEventListener('click', () => {
+        displayNone();
+        enlargeImage(image);
+    })
+})
